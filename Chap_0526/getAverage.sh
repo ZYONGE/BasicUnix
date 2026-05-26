16dd77b2258a7c0a69407b9fa6d8ecccc200150c
Jiyong Kim <kimjiyong0819@gmail.com>
2026-05-26 17:04:02 +0900

    Update Chap_0526

--- Chap_0526/getAverage.sh
+++ Chap_0526/getAverage.sh
@@ -1,23 +1,13 @@
-
-
-if [ $# -eq 0 ]; then
-    echo "사용:"
-    exit 1
-fi
-
 sum=0
 count=0
-
-
+ 
 for num in "$@"; do
-    # 숫자 유효성 검사
-
-    echo "  $num"
+    echo "$num"
     sum=$((sum + num))
     count=$((count + 1))
 done
-
+ 
 average=$(echo "scale=2; $sum / $count" | bc)
-
-echo "  합   계   : $sum"
-echo "  평   균   : $average"
+echo "합계: $sum"
+echo "평균: $average"
+ 
--- Chap_0526/getOddNum.sh
+++ Chap_0526/getOddNum.sh
@@ -2,11 +2,11 @@ i=1
 sum=0
  
 while [ $i -le 100 ]; do
-    if [ $((i % 2)) -eq 0 ]; then
+    if [ $((i % 2)) -eq 1 ]; then
         sum=$((sum + i))
     fi
     i=$((i + 1))
 done
  
-echo "1~100 짝수의 합: $sum"
+echo "1~100 홀수의 합: $sum"
  
